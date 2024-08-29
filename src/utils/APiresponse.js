7a6f13ba5c0e40c413d386a9f06e5fd8c1ef1d1c
Shivam pathak <shivampathak7789@gmail.com>
2024-08-29 15:45:02 +0530

    chnages related to utils and api

---
+++ src/utils/APiresponse.js
@@ -0,0 +1,8 @@
+class APiresponse{
+    constructor(statusCode , data , message = "Success"){
+        this.statusCode = statusCode
+        this.data = data 
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class APIError extends Error{
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack= ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.sucess = false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack 
+        }
+        else{
+            Error.captureStackTrace(this , this.constructor)
+        }
+    }
+}
+
+export {APIError}
