17a66307318e0d39f819086961fc1b9dbea55a01
Shivam pathak <shivampathak7789@gmail.com>
2024-09-01 16:10:07 +0530

    route setup

--- src/app.js
+++ src/app.js
@@ -15,5 +15,16 @@ app.use(express.urlencoded({extended: true , limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users" , userRouter)
+
+// http://localhost:3000/api/v1/users/register
+
+app.use((req, res, next) => {
+    res.status(404).send("Not Found");
+});
 
 export { app } 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
-
+import express from "express"
 dotenv.config({
     path: './env'
 })
 
+const app = express()
+
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000 , () => {
+    app.listen(process.env.PORT || 3000 , () => {
         console.log(`server is running at port: ${process.env.PORT}`);
     })
 })
